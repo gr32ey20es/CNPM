2fe8ce16f1f5a645c0ec66343a44d1299c6f15be
Greyes <gr32ey20es@gmail.com>
2023-10-27 07:36:43 +0700

    Update missing DB

--- database/dump.sql
+++ database/dump.sql
@@ -1,7 +1,14 @@
-CREATE TABLE data (
-  mssv INT,
-  ten VARCHAR(255)
-); 
+-- Adminer 4.8.1 PostgreSQL 16.0 (Debian 16.0-1.pgdg120+1) dump
 
-INSERT INTO data (mssv, ten)
-  VALUES (20215409, 'Kim'), (20210444, 'Huy');
+DROP TABLE IF EXISTS "account";
+CREATE TABLE "public"."account" (
+    "email" character varying(255),
+    "password" character varying(30),
+    "role" character varying(30)
+) WITH (oids = false);
+
+INSERT INTO "account" ("email", "password", "role") VALUES
+('kim@gmail.com',	'kim',	'leader'),
+('culi@gmail.com',	'culi',	'culi');
+
+-- 2023-10-27 00:35:58.774794+00
